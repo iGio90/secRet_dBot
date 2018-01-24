e47e89c48ad202210c322a3c0fdd11d73db458db
iGio90 <giovanni.rocca.90@gmail.com>
2018-01-24 12:50:02 +0100

    added weather

--- api/accuweather.py
+++ api/accuweather.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import discord
 import json
 import requests
@@ -11,13 +13,41 @@ async def on_message(message, discord_client):
     parts = message.content.split(" ")
     try:
         location_code = int(message[1])
-    except Exception as e:
-        if len(parts) < 1:
-            embed = utils.simple_embed('accuweather', 'use  !weather search *city_name to get the city code',
+        r = requests.get("https://api.accuweather.com/currentconditions/v1/" + str(location_code) +
+                         "?apikey=" + API_KEY + "&details=true&getphotos=true")
+        j = json.loads(r.content.decode('utf8'))
+        if 'code' in j:
+            embed = utils.simple_embed('accuweather', 'location id not found. use '
+                                                      '!weather search *city_name to get the city code',
                                        utils.random_color())
             await discord_client.send_message(message.channel, embed=embed)
         else:
-            if len(parts) > 2 and parts[2] == 'search':
+            obj = j[0]
+            embed = utils.simple_embed('accuweather', 'weather conditions',
+                                       utils.random_color())
+            embed.add_field(name='condition', value=obj['WeatherText'], inline=False)
+            embed.add_field(name='update time', value=datetime.fromtimestamp(obj['EpochTime'])
+                            .strftime('%Y-%m-%d %H:%M:%S'), inline=False)
+            ct = obj['Temperature']['Metric']['Value']
+            cf = obj['Temperature']['Imperial']['Value']
+            ct_r = obj['RealFeelTemperature']['Metric']['Value']
+            cf_r = obj['RealFeelTemperature']['Imperial']['Value']
+            embed.add_field(name='temperature', value=str(ct) + '**C** - ' + str(cf) + '**F**')
+            embed.add_field(name='real feel', value=str(ct_r) + '**C** - ' + str(cf_r) + '**F**')
+            embed.add_field(name='humidity', value=str(obj['RelativeHumidity']) + '%')
+            embed.add_field(name='cloud cover', value=str(obj['CloudCover']) + '%')
+            embed.add_field(name='wind direction', value=str(obj['Wind']['Direction']['Degrees']) +
+                                                     obj['Wind']['Direction']['English'])
+            embed.add_field(name='wind speed', value=str(obj['Wind']['Speed']['Metric']['value']) + ' km/h')
+            embed.set_thumbnail(obj['Photos'][0]['PortraitLink'])
+            await discord_client.send_message(message.channel, embed=embed)
+    except Exception as e:
+        if len(parts) < 1:
+            embed = utils.simple_embed('accuweather', 'use !weather search *city_name to get the city code',
+                                       utils.random_color())
+            await discord_client.send_message(message.channel, embed=embed)
+        else:
+            if len(parts) > 2 and parts[1] == 'search':
                 r = requests.get("https://api.accuweather.com/locations/v1/cities/autocomplete.json?apikey="
                                  + API_KEY + "&language=en&q=" + "milan")
                 j = json.loads(r.content.decode('utf8'))
