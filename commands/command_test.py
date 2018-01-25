924ba11ccb9ecfd0cc45cf715d55b6c994c40c6d
iGio90 <giovanni.rocca.90@gmail.com>
2018-01-25 04:25:58 +0100

    test code from discord

--- commands/command_test.py
+++ commands/command_test.py
@@ -4,11 +4,17 @@ import requests
 async def on_message(message, discord_client, mongo_db, bus, git_client, git_repo):
     parts = message.content.split(" ")
     if len(parts) > 1:
-        file_url = parts[1]
-        r = requests.get(file_url)
-        if r.status_code == 200:
-            cmd = r.content.decode('utf8')
-            print(cmd)
+        cmd = None
+        sub = str(parts[1])
+        if sub.startswith('http'):
+            file_url = parts[1]
+            r = requests.get(file_url)
+            if r.status_code == 200:
+                cmd = r.content.decode('utf8')
+        elif sub.startswith("```"):
+            cmd = str.join(' ', parts[2:])
+            cmd = cmd.replace('```', '')
+        if cmd is not None:
             exec(cmd)
             await run(message=message, discord_client=discord_client, mongo_db=mongo_db,
                       bus=bus, git_client=git_client, git_repo=git_repo)
