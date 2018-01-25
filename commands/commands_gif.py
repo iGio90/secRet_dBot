27c164c58187cbf41b85bbcca52305aad49879bc
iGio90 <giovanni.rocca.90@gmail.com>
2018-01-25 18:17:39 +0100

    fix gif search and add offset

--- commands/commands_gif.py
+++ commands/commands_gif.py
@@ -22,13 +22,20 @@ async def on_message(message, discord_client, bus):
                 await discord_client.send_message(message.channel, embed=embed)
         elif parts[1] == 'search':
             try:
-                q = str.join(" ", parts[2:])
-                r = requests.get('http://api.giphy.com/v1/gifs/search?api_key=' + API_KEY + '&q=' + q + '&limit=4')
+                q = str.join(" ", parts[2])
+                off = 0
+                if len(parts) > 3:
+                    try:
+                        off = int(parts[3])
+                    except Exception as e:
+                        pass
+                r = requests.get('http://api.giphy.com/v1/gifs/search?api_key=' + API_KEY + '&q=' + q +
+                                 '&limit=1&offset=' + str(off))
                 if r.status_code == 200:
                     rs = json.loads(r.content.decode('utf8'))
                     for item in rs['data']:
                         embed = utils.simple_embed(item['slug'], item['source'], utils.random_color())
-                        embed.set_image(url=rs['images']['original']['url'])
+                        embed.set_image(url=item['images']['original']['url'])
                         await discord_client.send_message(message.channel, embed=embed)
             except Exception as e:
                 pass
